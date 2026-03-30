9a594ee01e9fc45f72e6b6700b485af2f09b90fe
project <z480648150@163.com>
2026-03-31 00:18:10 +0800

    fix: 更新路径以正确加载字体和资源

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Project163.github.io',
-	base: 'blog',
+	base: '/blog',
 });
